3d9e4782c1043e19444ea9fecaf5e323be2c0a7f
unknown <web.faisal.bd@gmail.com>
2022-01-10 19:18:03 +0600

    user, review post and load

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ async function run() {
         const usersCollection = database.collection('users');
         const reviewCollection = database.collection('review');
         const buyerCollection = database.collection('buyer');
-       
 
 
 
@@ -38,7 +37,14 @@ async function run() {
             res.send(services);
         });
 
-     
+        // GET API for Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
 
         // POST API for services
         app.post('/services', async (req, res) => {
@@ -50,17 +56,68 @@ async function run() {
             res.json(result)
         });
 
-         // GET API for Single Service
-         app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log('getting specific service', id);
-            const query = { _id: ObjectId(id) };
-            const service = await servicesCollection.findOne(query);
-            res.json(service);
+        // DELETE API for delete services from manage all products
+           app.delete('/services/:id', async (req, res) => {
+               const id = req.params.id;
+               const query = { _id: ObjectId(id) };
+               const result = await servicesCollection.deleteOne(query);
+               console.log(result);
+
+               res.json(result);
+})
+
+        // POST API users 
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+       
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+
+
+        })
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
 
 
-            // post api for buyer
+         // POST API for review
+         app.post('/review', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+
+            const result = await reviewCollection.insertOne(service);
+            console.log(result);
+            res.json(result)
+        });
+
+         // GET API for review
+         app.get('/review', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+         // post api for buyer
        app.post('/buyer',async(req,res)=>{
             
         const user =req.body;
@@ -72,13 +129,39 @@ async function run() {
     });
 
 
-     // GET API for buyer
-     app.get('/buyer', async (req, res) => {
+    // GET API for buyer
+    app.get('/buyer', async (req, res) => {
         const user = buyerCollection.find({});
         const getUser = await user.toArray();
         console.log(getUser);
         res.send(getUser);
     });
+
+
+    // approved the status
+  app.put("/buyer/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    const filter = { _id: ObjectId(id) };
+    const updateDoc = {
+        $set: data,
+    };
+    const result = await buyerCollection.updateOne(filter, updateDoc);
+    res.json(result);
+});
+
+
+// DELETE API
+app.delete('/buyer/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await buyerCollection.deleteOne(query);
+    res.json(result);
+})
+
+
+
 
       
 
